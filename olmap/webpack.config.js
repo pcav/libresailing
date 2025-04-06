7497c8b52f3782bc94b556ab11f44ab3888eef23
enrico ferreguti <enricofer@gmail.com>
2025-04-07 00:06:26 +0200

    serving public files

--- olmap/webpack.config.js
+++ olmap/webpack.config.js
@@ -6,4 +6,10 @@ module.exports = {
     path: __dirname,
     filename: 'bundle.js'
   },
+  devServer: {
+    static: { 
+      directory: path.resolve(__dirname, './assets'), 
+      publicPath: '/assets'
+    }
+  }
 };
